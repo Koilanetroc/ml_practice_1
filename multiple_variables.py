ab8e2b64dbde99aa0407d201d39970a5f7b186bf
OlegT <koilanetroc@gmail.com>
2018-06-29 20:11:36 +0700

    initial commit

---
+++ multiple_variables.py
@@ -0,0 +1,58 @@
+from __future__ import division
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('training_data2.txt',names=['feet', 'rooms', 'price']).astype('float64')
+
+# mean normalization
+max_feet = data['feet'].max()
+avg_feet = data['feet'].mean()
+max_rooms = data['rooms'].max()
+avg_rooms = data['rooms'].mean()
+for row in data.values:
+    row[0] = (row[0] - avg_feet) / max_feet
+    row[1] = (row[1] - avg_rooms) / max_rooms
+print(data)
+
+# suppose that every row has format x1,x2,x3,x4,...,xn,y
+# we don't subtract one, because we'll need a bias node
+total_variables = len(data.values[0])
+thetas = [0] * total_variables
+alfa = 0.01
+
+# hypothesis
+def h(variables):
+    sum = 0
+    for i in range(len(variables)):
+        # we use i + 1 becaise of bias node
+        sum += variables[i] * thetas[i + 1]
+    return thetas[0] + sum
+
+def cost_function(data):
+    return (1/len(data.values) * 2) * sum([(h(row[:-1]) - row[-1])**2 for row in data.values])
+
+cost = cost_function(data)
+print('Cost function = %s' % cost)
+
+for k in range(1500):
+    print('=============================')
+    new_thetas = [0] * total_variables
+    for j in range(len(thetas)):
+        # bias node
+        if j == 0:
+            errors = [h(row[:-1]) - row[-1] for row in data.values]
+        else:
+            errors = [(h(row[:-1]) - row[-1]) * row[j-1] for row in data.values]
+        new_thetas[j] = thetas[j] - alfa * (1/len(data.values)) * sum(errors)
+    thetas = new_thetas
+    cost = cost_function(data)
+    print('New cost = %s' % cost)
+print('Thetas: %s' % thetas)
+
+print('Check results:')
+for row in data.values:
+    predicted = h(row[:-1])
+    print('=============================')
+    print('Predicted: %s' % predicted)
+    print('Real value: %s' % row[-1])
---
+++ one_variable.py
@@ -0,0 +1,45 @@
+from __future__ import division
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+theta0 = 0
+theta1 = 0
+alfa = 0.01
+
+# hypothesis
+def h(x,theta0,theta1):
+    return theta0 + theta1 * x
+
+# our cost function
+def cost_function(data):
+    cost = 0
+    return sum([(h(row[0], theta0, theta1) - row[1])**2 for row in data])/(len(data) * 2)
+
+data = pd.read_csv('training_data1.txt')
+cost = cost_function(data.values)
+print('Cost function = %f' % cost)
+
+difference = 99999999
+# repeat until convergence
+while difference > 10**(-15):
+    errors0 = [h(row[0], theta0, theta1) - row[1] for row in data.values]
+    errors1 = [(h(row[0], theta0, theta1) - row[1]) * row[0] for row in data.values]
+    new_theta0 = theta0 - alfa * (1/len(data.values)) * sum(errors0)
+    new_theta1 = theta1 - alfa * (1/len(data.values)) * sum(errors1)
+    theta0 = new_theta0
+    theta1 = new_theta1
+    new_cost = cost_function(data.values)
+    difference = cost - new_cost
+    cost = new_cost
+    print(cost)
+
+print('Finished learning')
+print(theta0,theta1)
+
+# test on training data(know that's bad)
+for row in data.values:
+    print('========================')
+    predicted = h(row[0],theta0,theta1)
+    print('Predicted value %s' % predicted)
+    print('Real value %s' % row[1])
